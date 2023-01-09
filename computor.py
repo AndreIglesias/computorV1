15fce0e6c63b3f7b730c803a860e92f7517c9aaf
AndreIglesias <>
2023-01-09 16:41:40 +0100

    separated classes

--- computor.py
+++ computor.py
@@ -6,7 +6,7 @@
 #    By: ciglesia <ciglesia@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/12/12 15:45:24 by ciglesia          #+#    #+#              #
-#    Updated: 2022/12/16 20:11:59 by ciglesia         ###   ########.fr        #
+#    Updated: 2023/01/09 16:14:32 by ciglesia         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,11 +14,9 @@ import sys
 import re
 import copy
 from pprint import pprint
+from cpmath import CPMath, symbol_table, symbol_op, symbol_friend, symbol_exp
 
-symbol_table = {'x', 'X', '^', '+', '-', '*', '/', '=', '.'}
-symbol_op = {'+', '-', '*', '/'}
-symbol_friend = {'x', 'X'}
-symbol_exp = "⁰¹²³⁴⁵⁶⁷⁸⁹"
+cpm = CPMath()
 
 class Polynome(object):
     def __init__(self, eq: str):
@@ -28,91 +26,14 @@ class Polynome(object):
         self.ast_right = None
         self.equation = {}
         self.__parser()
-        self.reduced_left = self.__dictionary(self.ast_left)
-        self.reduced_right = self.__dictionary(self.ast_right)
+        self.reduced_left = cpm.poly_dictionary(self.ast_left)
+        self.reduced_right = cpm.poly_dictionary(self.ast_right)
         self.reduced = None
         self.__reduce_d()
         self.degree = 0
         if len(self.reduced) != 0:
             self.degree = max(self.reduced)
         self.print_info()
-
-    def __print_exponent(self, exponent: int):
-        xp = "⁾"
-        sign = ""
-        if exponent < 0:
-            sign = "⁻"
-            exponent *= -1
-        while exponent > 0:
-            xp += symbol_exp[int(abs(exponent % 10))]
-            exponent //= 10
-        xp += sign
-        xp += "⁽"
-        print(xp[::-1], end="")
-
-    def __complex_round(self, x: float or complex) -> float or complex:
-        if isinstance(x, complex):
-            return x # complex(round(x.real, 4),round(x.imag, 4))
-        return x # (round(x, 4))
-
-    def __format_roots(self, x: float or complex) -> str:
-        if isinstance(x, complex):
-            a, b = x.real.as_integer_ratio()
-            ai, bi = x.imag.as_integer_ratio()
-            if b != 1 and -1000 < a and a < 1000 and -1000 < b and b < 1000:
-                sab = "{:}/{:}".format(a, b)
-            else:
-                sab = "{:}".format(x.real)
-
-            if bi != 1 and -1000 < ai and ai < 1000 and -1000 < bi and bi < 1000:
-                sabi = "{:}/{:}j".format(ai, bi)
-            else:
-                sabi = "{:}j".format(x.imag)
-            if x.imag < 0:
-                return (sab + sabi)
-            return (sab + "+" + sabi)
-
-        a, b = x.as_integer_ratio()
-        if b != 1 and -1000 < a and a < 1000 and -1000 < b and b < 1000:
-            sab = "{:}/{:}".format(a, b)
-        else:
-            sab = ""
-        return (str(x) + " or " + sab)
-
-    def __quadratic(self, a: int or float, b: int or float, c: int or float) -> tuple:
-        print("Steps:")
-        print()
-        a, b, c = self.__complex_round(a), self.__complex_round(b), self.__complex_round(c)
-        print("x = ( -b ± sqrt(b⁽²⁾ - 4(a)(c)) ) / 2(a)")
-        print()
-        print("a =", a)
-        print("b =", b)
-        print("c =", c)
-        print()
-        print("x = ( -({:}) ± sqrt({:}⁽²⁾ - 4({:})({:})) ) / 2({:})".format(b, b, a, c, a))
-        b2 = self.__complex_round(b ** 2)
-        ac4 = self.__complex_round(4 * a * c)
-        a2 = self.__complex_round(2 * a)
-        print("x = ( -({:}) ± sqrt(({:}) - ({:})) ) / {:}".format(b, b2, ac4, a2))
-        print("x = ( -({:}) ± sqrt({:}) ) / {:}".format(b, b2 - ac4, a2))
-        sq = self.__complex_round((b2 - ac4)**(1/2))
-        print("x = ( -({:}) ± {:} ) / {:}".format(b, sq, a2))
-        print("x1 = ( -({:}) - {:} ) / {:}".format(b, sq, a2))
-        print("x2 = ( -({:}) + {:} ) / {:}".format(b, sq, a2))
-        print("x1 = {:} / {:}".format((-b) - sq, a2))
-        print("x2 = {:} / {:}".format((-b) + sq, a2))
-        print()
-        print("Solutions:")
-        x1 = self.__complex_round(((-b) - sq) / a2)
-        if abs(x1) == 0:
-            x1 = 0
-        print(self.__format_roots(x1))
-        x2 = self.__complex_round(((-b) + sq) / a2)
-        if abs(x2) == 0:
-            x2 = 0
-        print(self.__format_roots(x2))
-        return (x1, x2)
-
 
     def __print_reduced(self):
         first = True
@@ -133,13 +54,13 @@ class Polynome(object):
                     if key != 0:
                         print(" * ", end="")
             else:
-                print(self.__complex_round(abs(self.reduced[key]), 4), end="")
+                print(abs(self.reduced[key]), end="")
                 if key != 0:
                     print(" * ", end="")
             if key != 0:
                 print("x", end="")
             if key != 0 and key != 1:
-                self.__print_exponent(key)
+                cpm.print_exponent(key)
             first = False
         print(" = 0")
 
@@ -170,23 +91,18 @@ class Polynome(object):
             if 0 in self.reduced:
                 c = self.reduced[0]
             if a != 0:
-                self.__quadratic(a, b, c)
+                cpm.quadratic(a, b, c)
             elif b != 0:
                 if b != 1:
                     print("Steps:")
                     print("x = {:} / {:}".format(-c, b))
                 else:
                     print("Solution:")
-                x = self.__complex_round(-c / b)
+                x = -c / b
                 if abs(x) == 0:
                     print("X = 0")
                 else:
-                    a, b = x.as_integer_ratio()
-                    if b != 1 and (-1000 < a and a < 1000) and \
-                       (-1000 < b and b < 1000):
-                        print("x = {:} or {:}/{:}".format(x, a, b))
-                    else:
-                        print("x = {:}".format(x))
+                    print("x = " + cpm.format_roots(x))
             elif len(self.reduced) == 0:
                 print("No roots to solve")
             else:
@@ -219,27 +135,12 @@ class Polynome(object):
             x[0][i] = (tup, x_op)
         return (x)
 
-    def __valid_coeff(self, n: str) -> bool:
-        try:
-            float(n)
-            return (True)
-        except ValueError:
-            if len(n) == 0 or n[0] != 'X' or len(n) > 1 and n[1] != '^' or len(n) == 2:
-                return (False)
-            elif len(n) > 2:
-                try:
-                    int(n[2:])
-                except ValueError:
-                    return (False)
-                return (True)
-            return (True)
-
     def __syntax_structure(self, eq: str):
         for term in eq[0]:
             for coeff in term[0]:
                 if coeff == '':
                     raise ValueError("Synytax Error: Empty token next to symbol")
-                elif not self.__valid_coeff(coeff):
+                elif not cpm.valid_coeff(coeff):
                     raise ValueError("Synytax Error: Invalid token: {:}".format(coeff))
 
     def __parser(self):
@@ -278,76 +179,6 @@ class Polynome(object):
         self.ast_right = eq2
         self.ast = [copy.deepcopy(eq1), copy.deepcopy(eq2)]
 
-
-    def __reduce_term(self, term: tuple) -> tuple:
-        coeff = 1
-        degree = 0
-
-        # Initialize coeff or degree
-        if 'X' in term[0][0]:
-            degree = 1
-            if len(term[0][0]) != 1:
-                degree = int(term[0][0][2:])
-        else:
-            coeff = float(term[0][0])
-
-        # Calculate coeff and degree
-        i = 1
-        for op in term[1]:
-            if len(term[0]) <= i:
-                break
-            if op == '*':
-                ## if X without exponent (R01)
-                if 'X' in term[0][i]:
-                    if len(term[0][i]) == 1:
-                        degree += 1
-                    else:
-                        degree += int(term[0][i][2:])
-                else:
-                    coeff *= float(term[0][i])
-            elif op == '/':
-                if 'X' in term[0][i]:
-                    if len(term[0][i]) == 1:
-                        degree -= 1
-                    else:
-                        degree -= int(term[0][i][2:])
-                else:
-                    coeff /= float(term[0][i])
-            i += 1
-        return (degree, coeff)
-
-    def __dictionary(self, ast: list) -> dict:
-        """
-        return dictionary with { degree : coefficient }
-        {
-          X^degree: coeff,
-          0: coeff,
-          ...,
-          n: coeff,
-        }
-        """
-        new = {}
-
-        # Reduce terms
-        for t in range(len(ast[0])):
-            ast[0][t] = self.__reduce_term(ast[0][t])
-
-        # Reduce equation
-        new[ast[0][0][0]] = ast[0][0][1]
-        i = 1
-        for op in ast[1]:
-            if op == '+':
-                if ast[0][i][0] in new:
-                    new[ast[0][i][0]] += ast[0][i][1]
-                else:
-                    new[ast[0][i][0]] = ast[0][i][1]
-            elif op == '-':
-                if ast[0][i][0] in new:
-                    new[ast[0][i][0]] -= ast[0][i][1]
-                else:
-                    new[ast[0][i][0]] = -ast[0][i][1]
-            i += 1
-        return (new)
 
     def __reduce_d(self):
         self.reduced = copy.deepcopy(self.reduced_left)
---
+++ cpmath.py
@@ -0,0 +1,170 @@
+
+symbol_table = {'x', 'X', '^', '+', '-', '*', '/', '=', '.'}
+symbol_op = {'+', '-', '*', '/'}
+symbol_friend = {'x', 'X'}
+symbol_exp = "⁰¹²³⁴⁵⁶⁷⁸⁹"
+
+class CPMath(object):
+
+    # Displaying functions
+
+    def print_exponent(self, exponent: int):
+        xp = "⁾"
+        sign = ""
+        if exponent < 0:
+            sign = "⁻"
+            exponent *= -1
+        while exponent > 0:
+            xp += symbol_exp[int(abs(exponent % 10))]
+            exponent //= 10
+        xp += sign
+        xp += "⁽"
+        print(xp[::-1], end="")
+
+    def valid_coeff(self, n: str) -> bool:
+        try:
+            float(n)
+            return (True)
+        except ValueError:
+            if len(n) == 0 or n[0] != 'X' or len(n) > 1 and n[1] != '^' or len(n) == 2:
+                return (False)
+            elif len(n) > 2:
+                try:
+                    int(n[2:])
+                except ValueError:
+                    return (False)
+                return (True)
+            return (True)
+
+
+    # Computing Functions
+
+    def format_roots(self, x: float or complex) -> str:
+        if isinstance(x, complex):
+            a, b = x.real.as_integer_ratio()
+            ai, bi = x.imag.as_integer_ratio()
+            if b != 1 and -1000 < a and a < 1000 and -1000 < b and b < 1000:
+                sab = "{:}/{:}".format(a, b)
+            else:
+                sab = "{:}".format(x.real)
+
+            if bi != 1 and -1000 < ai and ai < 1000 and -1000 < bi and bi < 1000:
+                sabi = "{:}/{:}j".format(ai, bi)
+            else:
+                sabi = "{:}j".format(x.imag)
+            if x.imag < 0:
+                return (sab + sabi)
+            return (sab + "+" + sabi)
+
+        a, b = x.as_integer_ratio()
+        print("dbg:",a, b,x)
+        if b != 1 and -1000 < a and a < 1000 and -1000 < b and b < 1000:
+            sab = "{:}/{:}".format(a, b)
+        else:
+            sab = None
+        if sab != None:
+            return (str(x) + " or " + sab)
+        return (str(x))
+
+    def quadratic(self, a: int or float, b: int or float, c: int or float) -> tuple:
+        print("Steps:")
+        print()
+        print("x = ( -b ± sqrt(b⁽²⁾ - 4(a)(c)) ) / 2(a)")
+        print()
+        print("a =", a)
+        print("b =", b)
+        print("c =", c)
+        print()
+        print("x = ( -({:}) ± sqrt({:}⁽²⁾ - 4({:})({:})) ) / 2({:})".format(b, b, a, c, a))
+        b2 = b ** 2
+        ac4 = 4 * a * c
+        a2 = 2 * a
+        print("x = ( -({:}) ± sqrt(({:}) - ({:})) ) / {:}".format(b, b2, ac4, a2))
+        print("x = ( -({:}) ± sqrt({:}) ) / {:}".format(b, b2 - ac4, a2))
+        sq = (b2 - ac4)**(1/2)
+        print("x = ( -({:}) ± {:} ) / {:}".format(b, sq, a2))
+        print("x1 = ( -({:}) - {:} ) / {:}".format(b, sq, a2))
+        print("x2 = ( -({:}) + {:} ) / {:}".format(b, sq, a2))
+        print("x1 = {:} / {:}".format((-b) - sq, a2))
+        print("x2 = {:} / {:}".format((-b) + sq, a2))
+        print()
+        print("Solutions:")
+        x1 = ((-b) - sq) / a2
+        if abs(x1) == 0:
+            x1 = 0
+        print(self.format_roots(x1))
+        x2 = ((-b) + sq) / a2
+        if abs(x2) == 0:
+            x2 = 0
+        print(self.format_roots(x2))
+        return (x1, x2)
+
+    def reduce_term(self, term: tuple) -> tuple:
+        coeff = 1
+        degree = 0
+
+        # Initialize coeff or degree
+        if 'X' in term[0][0]:
+            degree = 1
+            if len(term[0][0]) != 1:
+                degree = int(term[0][0][2:])
+        else:
+            coeff = float(term[0][0])
+
+        # Calculate coeff and degree
+        i = 1
+        for op in term[1]:
+            if len(term[0]) <= i:
+                break
+            if op == '*':
+                ## if X without exponent (R01)
+                if 'X' in term[0][i]:
+                    if len(term[0][i]) == 1:
+                        degree += 1
+                    else:
+                        degree += int(term[0][i][2:])
+                else:
+                    coeff *= float(term[0][i])
+            elif op == '/':
+                if 'X' in term[0][i]:
+                    if len(term[0][i]) == 1:
+                        degree -= 1
+                    else:
+                        degree -= int(term[0][i][2:])
+                else:
+                    coeff /= float(term[0][i])
+            i += 1
+        return (degree, coeff)
+
+    def poly_dictionary(self, ast: list) -> dict:
+        """
+        return dictionary with { degree : coefficient }
+        {
+          X^degree: coeff,
+          0: coeff,
+          ...,
+          n: coeff,
+        }
+        """
+        new = {}
+
+        # Reduce terms
+        for t in range(len(ast[0])):
+            ast[0][t] = self.reduce_term(ast[0][t])
+
+        # Reduce equation
+        new[ast[0][0][0]] = ast[0][0][1]
+        i = 1
+        for op in ast[1]:
+            if op == '+':
+                if ast[0][i][0] in new:
+                    new[ast[0][i][0]] += ast[0][i][1]
+                else:
+                    new[ast[0][i][0]] = ast[0][i][1]
+            elif op == '-':
+                if ast[0][i][0] in new:
+                    new[ast[0][i][0]] -= ast[0][i][1]
+                else:
+                    new[ast[0][i][0]] = -ast[0][i][1]
+            i += 1
+        return (new)
